978418485c798e9612d77da9e1d90f8ad40cc772
이도현 <shuajjjjj@naver.com>
2024-08-10 15:23:04 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int f,b;
+    char c;
+    cin >> f >> c >> f >> c >> b;
+    cout << "010"<<c<<b<<c<<f;
+    return 0;
+}
