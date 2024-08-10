a379f52d1c4475fdf5561c417acb4d4ea20cb4f8
이도현 <shuajjjjj@naver.com>
2024-08-10 15:16:26 +0900

    Title: 날짜 변경하여 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int yyyy, mm ,dd;
+    char dot = '.', c;
+
+    cin >> mm >> c >> dd >> c >> yyyy;
+    cout << yyyy<<dot<<mm<<dot<<dd;
+    return 0;
+}
