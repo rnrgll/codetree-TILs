ecd4331120cfd099e7e9adcbbb7de84fe4e2d790
이도현 <shuajjjjj@naver.com>
2024-08-10 15:25:32 +0900

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a>> b;
+    //더했을 때 결과
+    cout << a+b<<endl;
+    //뺐을 때 결과
+    cout << a-b<< endl;
+    //나누었을 때 몫
+    cout << a/b<<endl;
+    //나머지 출력
+    cout << a%b;
+
+    return 0;
+}
