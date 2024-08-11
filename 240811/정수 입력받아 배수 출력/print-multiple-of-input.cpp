6d17056fb312a347c6d5fe60840f829788e6d5a1
이도현 <shuajjjjj@naver.com>
2024-08-11 13:18:17 +0900

    Title: 정수 입력받아 배수 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240811/정수 입력받아 배수 출력/print-multiple-of-input.cpp
+++ 240811/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -5,11 +5,16 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    for(int c=0, k=n; c<5; k++){
-        if(k%n==0){
-            cout << k << " ";
-            c++;
-        }
+    // for(int c=0, k=n; c<5; k++){
+    //     if(k%n==0){
+    //         cout << k << " ";
+    //         c++;
+    //     }
+    // }
+    
+    //더 간결한 코드로 가능
+    for(int i=n; i<=n*5; i+=n){
+        cout << i << " ";
     }
     return 0;
 }
