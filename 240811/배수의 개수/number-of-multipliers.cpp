dedda578896486cb10c0e6fa596a9bbf7bc474a7
이도현 <shuajjjjj@naver.com>
2024-08-11 15:18:36 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int cnt_3 = 0, cnt_5 = 0;
+    int n;
+
+    for(int i=0;i<10;i++){
+        cin >> n;
+        if(n%3==0) cnt_3++;
+        if(n%5==0) cnt_5++;    
+    }
+
+    cout << cnt_3 << " " << cnt_5;
+    return 0;
+}
