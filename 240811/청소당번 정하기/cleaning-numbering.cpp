e3f7155dcf60df148598b3362477eda09129f135
이도현 <shuajjjjj@naver.com>
2024-08-11 15:22:44 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240811/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int classroom = 0, corridor = 0, bathroom = 0;
+    int n;
+    cin >> n;
+    for(int i=1;i<n;i++){
+        if(i%12==0) {
+            bathroom++;
+        }
+        else if(i%6==0||i%3==0){
+            corridor++;
+        }
+        else if(i%2==0){
+            classroom++;
+        }
+    }
+
+    cout << classroom << " " << corridor << " " << bathroom;
+    return 0;
+}
