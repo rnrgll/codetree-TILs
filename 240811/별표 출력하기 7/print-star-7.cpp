e5192d3477bd25cc65ff418f532c5d59eb51628c
이도현 <shuajjjjj@naver.com>
2024-08-11 18:11:34 +0900

    Title: 별표 출력하기 7, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240811/별표 출력하기 7/print-star-7.cpp
+++ 240811/별표 출력하기 7/print-star-7.cpp
@@ -6,7 +6,7 @@ int main() {
     int n;
     cin >> n;
     for(int i=0;i<n;i++){
-        for(int j=0; j<i;j++)
+        for(int j=0; j<i+1;j++)
             cout <<"*"<< " ";
         cout << endl;
     }
