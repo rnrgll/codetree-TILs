7bbccc2438c38a8ebbd18615c84d2d46ab113679
이도현 <shuajjjjj@naver.com>
2024-08-11 14:05:44 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int i = 1;
+    while(i<=n){
+        cout << "*\n";
+        i++;
+    }
+    return 0;
+}
