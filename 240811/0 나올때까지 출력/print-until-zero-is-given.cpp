8d48f21427b420e6b3f7f710838b2741b32ba025
이도현 <shuajjjjj@naver.com>
2024-08-11 16:27:14 +0900

    Title: 0 나올때까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    while(true){
+        cin >> n;
+        if(n==0){
+            break;
+        }
+        cout << n << endl;
+    }
+    return 0;
+}
