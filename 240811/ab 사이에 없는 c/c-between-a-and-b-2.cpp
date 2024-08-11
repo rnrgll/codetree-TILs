500c8b2941e3e780a229851ab153fc6b77a39322
이도현 <shuajjjjj@naver.com>
2024-08-11 17:53:37 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    bool flag = false;
+    cin >> a >> b >> c;
+    for(int i =a; i<=b; i++){
+        if(i%c==0){
+            flag = true;
+            break;
+        }
+    }
+    cout << (!flag?"YES":"NO");
+    return 0;
+}
