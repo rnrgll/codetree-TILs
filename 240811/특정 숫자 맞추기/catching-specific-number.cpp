f7c45c93694e34bdef82174dd62a47cf27684422
이도현 <shuajjjjj@naver.com>
2024-08-11 16:28:58 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/특정 숫자 맞추기/catching-specific-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    while(true){
+        cin >> n;
+        if(n==25){
+            cout << "Good";
+            break;
+        }
+        else if(n<25){
+            cout << "Higher" << endl;
+        }
+        else {
+            cout << "Lower" << endl;
+        }
+
+    }
+    return 0;
+}
