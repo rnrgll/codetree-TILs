f4298317ba9db17d0b734f520720e24589f57b57
이도현 <shuajjjjj@naver.com>
2024-08-11 16:17:12 +0900

    Title: 조건에 부합하는 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    for(int i=1; i<=a; i++){
+        if(i%2==0&&i%4!=0) continue;
+        else if((i/8)%2==0) continue;
+        else if((i%7)<4) continue;
+        else {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
