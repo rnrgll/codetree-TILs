637c6b71ef4c965ff08de9b321f6b17a0fae0035
이도현 <shuajjjjj@naver.com>
2024-08-12 22:53:16 +0900

    Title: 별표 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240812/별표 출력하기 3/print-star-3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            cout << " " << " ";
+        }
+
+        for(int k=0; k<2*(n-i)-1;k++){
+            cout << "*" << " ";
+        }
+
+        for(int j=0; j<i; j++){
+            cout << " " << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
