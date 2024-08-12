ee58aabc58b60c7f5e89072116bac770bbb9af17
이도현 <shuajjjjj@naver.com>
2024-08-12 23:32:04 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240812/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
+++ 240812/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -16,7 +16,7 @@ int main() {
     for(int i=n-1; i>=1; i--){
         //i번째 열에서 공백은 n-i, 문자는 i개
         for(int j=0; j<i; j++) cout << "@ ";
-        for(int j=0; j<n-i; j++) cout << "  ";
+        //for(int j=0; j<n-i; j++) cout << "  "; //출력할 필요 없음
         cout << endl;
     }
 
