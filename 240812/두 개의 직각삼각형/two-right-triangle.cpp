f920fea6db76ea5d36626470e3d8427654af4595
이도현 <shuajjjjj@naver.com>
2024-08-12 23:02:17 +0900

    Title: 두 개의 직각삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240812/두 개의 직각삼각형/two-right-triangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for(int i=n;i>0;i--){
+        for(int j=0; j<i; j++) cout <<"*";
+
+        for(int k=0; k<n-i;k++) cout << "  ";
+
+        for(int j=0; j<i; j++) cout <<"*";
+        cout << endl;
+        
+    }
+    return 0;
+}
