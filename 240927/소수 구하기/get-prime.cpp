3a8cb406080347a93ed6b0292924545663151ba6
이도현 <shuajjjjj@naver.com>
2024-09-27 00:09:22 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240927/소수 구하기/get-prime.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    bool flag;
+    cin >> n;
+
+    for(int i=2; i<=n; i++){
+        flag = false;
+        for(int j=2; j<i; j++){
+            if(i%j==0) {
+                flag = true;
+                break;
+            }
+        }
+
+        if(!flag) cout << i << " ";
+    }
+
+    return 0;
+}
