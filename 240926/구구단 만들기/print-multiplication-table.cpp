216fb138f88afce9625ca546f2b7e8b53022196c
이도현 <shuajjjjj@naver.com>
2024-09-26 13:57:48 +0900

    Title: 구구단 만들기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240926/구구단 만들기/print-multiplication-table.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    for(int i=1; i<=9; i++){
+        for(int j=b; j>=a; j-=2){
+            cout << j << " * " << i << " = " << j*i << " ";
+            if(j!=a) cout << "/ ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
