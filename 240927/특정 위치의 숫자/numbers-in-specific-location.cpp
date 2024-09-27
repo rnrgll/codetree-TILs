e3c274baea065124b49b6a1aa9f3b2ae9f081e0c
이도현 <shuajjjjj@naver.com>
2024-09-27 20:50:03 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240927/특정 위치의 숫자/numbers-in-specific-location.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int sum = 0;
+    int arr[10];
+    for(int i=0; i<10; i++){
+        cin >> arr[i];
+    }
+
+    sum = arr[2] + arr[4] + arr[9];
+    cout << sum;
+    return 0;
+}
