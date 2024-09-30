13fbfbd2a78fcd6659d4471f00efbcc38c53a41b
이도현 <shuajjjjj@naver.com>
2024-09-30 12:30:34 +0900

    Title: 숫자 직사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240930/숫자 직사각형/rectangle-of-numbers.cpp
+++ 240930/숫자 직사각형/rectangle-of-numbers.cpp
@@ -5,15 +5,22 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int cnt=0;
     int n,m;
+    int arr[100][100];
     cin >> n >> m;
 
+    //이차원 배열 써서 해결하기
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            arr[i][j] = ++cnt;
+        }
+    }
 
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            cnt++;
-            cout << cnt << " ";
+            cout << arr[i][j] << " ";
         }
         cout << endl;
     }
+
     return 0;
 }
