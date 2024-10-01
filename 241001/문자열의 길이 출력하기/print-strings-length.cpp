4b721e614ee78eb531e8e59beee2e0d91ec45145
이도현 <shuajjjjj@naver.com>
2024-10-01 22:12:08 +0900

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241001/문자열의 길이 출력하기/print-strings-length.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str1, str2;
+    cin >> str1 >> str2;
+    cout << str1.length() + str2.length();
+    return 0;
+}
