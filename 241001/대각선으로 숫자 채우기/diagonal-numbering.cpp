e5281c1d7e675958c75677ba8fe48ddaeaf9108c
이도현 <shuajjjjj@naver.com>
2024-10-01 21:29:56 +0900

    Title: 대각선으로 숫자 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241001/대각선으로 숫자 채우기/diagonal-numbering.cpp
+++ 241001/대각선으로 숫자 채우기/diagonal-numbering.cpp
@@ -2,31 +2,30 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    //다른 풀이로 접근
     int n, m, cnt= 1;
     int arr[100][100]={};
 
+    //입력
     cin >> n >> m;
-    for(int i=0; i<m; i++){
-        int start_column = i;
-        for(int j=0; j<n; j++){
-            if(start_column<0){
-                break;
+
+
+    //연산
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(arr[i][j]!=0) continue;
+
+            //비어있다면 해당 지점을 시작점으로 대각선으로 이동시킨다.
+            int start_row = i, start_col = j;
+            while(start_row<n && start_col>=0){
+                arr[start_row++][start_col--] = cnt++;
             }
-            arr[j][start_column--] = cnt++;
         }
     }
 
-    for(int i=1; i<n; i++){
-        int start_row = i;
-        for(int j=m-1; j>=0; j--){
-            if(start_row>=n){
-                break;
-            }
-            arr[start_row++][j] = cnt++;
-        }
-    }
 
+
+    //출력
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
             cout<<arr[i][j] << " ";
