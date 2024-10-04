864573f3cfe6aa02f2dd0a6d10ab8b3bd571c447
rnrgll <shuajjjjj@naver.com>
2024-10-04 19:23:59 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241004/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+
+    for(int i=0; i<10; i++){
+        cin >> str;
+        cout << str << endl;
+    }
+    return 0;
+}
