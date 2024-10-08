6866fbed4f0ee82274885b1b91adc56f31d87119
rnrgll <shuajjjjj@naver.com>
2024-10-08 15:47:43 +0900

    Title: 정수의 최솟값, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241008/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int findMin(int a, b, c){
+    int mNum = min(a,b);
+    mNum = min(mNum, c);
+
+    return mNum;
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b ,c;
+
+    cin >> a >> b >> c;
+
+    cout << findMin(a,b,c);
+
+    return 0;
+}
